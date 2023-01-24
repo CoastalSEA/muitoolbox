f461917adcb6c87a0a8e7b03799fa04a1d9f4618
(no author) <(no author)@2f3b7377-8f70-4b58-a42b-65e782757488>
2023-01-24 11:20:19 +0000

    Bug in inputUI.m and added check for toolbox licences
    git-svn-id: svn://192.168.0.80/muitoolbox@96 2f3b7377-8f70-4b58-a42b-65e782757488

---
+++ Trunk/muifunctions/check4toolbox.m
@@ -0,0 +1,29 @@
+function isok = check4toolbox(feature_name)
+%
+%-------function help------------------------------------------------------===
+% NAME
+%   check4addon.m
+% PURPOSE
+%   Check whether a toolbox is available
+% USAGE
+%   isok = check4toolbox(name)
+% INPUT
+%   feature_name - feature name of toolbox to be checked (usually name with
+%   underscores).
+% OUTPUT
+%   isok - ture if toolbox is available
+% NOTES
+%   https://uk.mathworks.com/matlabcentral/answers/377731-how-do-features-from-license-correspond-to-names-from-ver#answer_300675
+% SEE ALSO
+%	initialise_mui_app.m
+%
+% Author: Ian Townend
+% CoastalSEA (c) Dec 2022
+%--------------------------------------------------------------------------
+% 
+    if license('test', feature_name)
+        isok = 1;
+    else
+        isok = 0;
+    end
+end
